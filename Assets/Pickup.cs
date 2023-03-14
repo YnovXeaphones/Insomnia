db6d5edbeeb85cb29c437ad4663a4dc6c753d934
YVelay <yohan.velay@ynov.com>
2023-03-14 09:24:40 +0100

    Added Score + UI

---
+++ Assets/Pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] UnityEvent _addPretzelScore;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            _addPretzelScore.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
